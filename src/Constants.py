b02e925f12b9631c007d63fb5ea5908100ad858e
dionizijefa <dionizije.fa@hotmail.com>
2023-08-06 17:22:05 +0200

    Gas calculations done

---
+++ src/Constants.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+@dataclass
+class Constants:
+    population_2011 = 35312
+    diesel_ton_to_litre = 1176.5
+    petrol_ton_to_litre = 1388.9
+    diesel_litre_to_kwh = 10.96
+    petrol_litre_to_kwh = 9.61
+    lpg_ton_to_litre = 1969
+    lpg_litre_to_kwh = 7.08
--- src/energy_consumption.py
+++ src/energy_consumption.py
@@ -2,14 +2,18 @@ import seaborn as sns
 import pandas as pd
 from pathlib import Path
 import matplotlib.pyplot as plt
+from src.Constants import Constants
 
 root_dir = Path(__file__).parents[1]
 
-class EnergyCharts:
+constant = Constants()
+
+
+class Energy2011:
     sns.set_style()
 
     def __init__(self):
-        self.n_residents_2011 = 35.312
+        self.n_residents_2011 = 35312
         self.colors = sns.color_palette('pastel')[0:5]
         self.heating_2011 = pd.read_csv(
             root_dir / 'data/vinkovci_grijanje_2011.csv'
@@ -68,10 +72,17 @@ class EnergyCharts:
 
     def gas_by_type(self):
         private_cars = self.private_cars_2011
+        city_cars = self.city_cars_2011
+        private_cars.fillna(0)
+        city_cars_petrol = (city_cars.iloc[0]['potrošnja_goriva(l)'] * constant.petrol_litre_to_kwh)
+        city_cars_diesel = (city_cars.iloc[1]['potrošnja_goriva(l)'] * constant.diesel_litre_to_kwh)
 
-        petrol_spent = private_cars['procijenjena_potrošena_masa_benzina(t)'].sum()
-        diesel_spent = private_cars['procijenjena_potrošena_masa_dizela(t)'].sum()
-        unp_spent = private_cars['procijenjena_potrošena_masa_unp(t)'].sum()
+        petrol_spent = private_cars['procijenjena_potrošena_masa_benzina(t)'].sum() * constant.petrol_litre_to_kwh * constant.petrol_ton_to_litre
+        diesel_spent = private_cars['procijenjena_potrošena_masa_dizela(t)'].sum() * constant.diesel_litre_to_kwh * constant.diesel_ton_to_litre
+        unp_spent = private_cars['procijenjena_potrošena_masa_unp(t)'].sum() * constant.lpg_ton_to_litre * constant.lpg_litre_to_kwh
+
+        petrol_spent = petrol_spent + city_cars_petrol
+        diesel_spent = diesel_spent + city_cars_diesel
 
         chart = plt.pie(
             [petrol_spent, diesel_spent, unp_spent],
@@ -89,20 +100,35 @@ class EnergyCharts:
     def gas_by_category(self):
         city_cars = self.city_cars_2011
         private_cars = self.private_cars_2011
+        private_cars = private_cars.fillna(0)
+        city_cars['energy'] = (city_cars.iloc[0]['potrošnja_goriva(l)'] * constant.petrol_litre_to_kwh) \
+                              + (city_cars.iloc[1]['potrošnja_goriva(l)'] * constant.diesel_litre_to_kwh)
+
+
 
         # convert city consumption in L to tonnes like in private data
-        city_cars_petrol = city_cars.loc[
-            city_cars["vrsta_goriva"] == "benzin"
-        ]['potrošnja_goriva(l)'] * 0.00085
-        city_cars_diesel = city_cars.loc[
-            city_cars["vrsta_goriva"] == "dizel"
-        ]['potrošnja_goriva(l)'] * 0.00072
+        private_cars['energy'] = (private_cars[
+                                       'procijenjena_potrošena_masa_benzina(t)'] * constant.petrol_ton_to_litre * constant.petrol_litre_to_kwh) \
+                                 + (private_cars[
+                                         'procijenjena_potrošena_masa_dizela(t)'] * constant.diesel_ton_to_litre * constant.diesel_litre_to_kwh) \
+                                 + (private_cars[
+                                         'procijenjena_potrošena_masa_unp(t)'] * constant.lpg_ton_to_litre * constant.lpg_litre_to_kwh)
+        private_cars.loc[private_cars['vrsta_prijevoza'] == 'osobna_vozila', 'energy'] += city_cars['energy']
+        consumption = private_cars.loc[~private_cars["procijenjena_potrošnja_goriva(kWh)"].isna()]
+        consumption.loc[consumption['vrsta_prijevoza'] == 'taxi', 'energy'] = consumption.loc[
+            consumption['vrsta_prijevoza'] == 'taxi'
+            ]['potrošnja_dizelskog_goriva(l)'] * constant.diesel_litre_to_kwh
 
-        potrošnja = private_cars.loc[~private_cars["procijenjena_potrošnja_goriva(kWh)"].isna()]
+        threshold = 2
+        consumption["percentage"] = 100 * consumption["energy"] / consumption["energy"].sum()
+        small_categories = consumption["percentage"] < threshold
+        consumption.loc[small_categories, "vrsta_prijevoza"] = "Taxi, autobusi, mopedi i motocikli"
+        grouped_consumption = consumption.groupby("vrsta_prijevoza").sum()
+
         chart = plt.pie(
-            potrošnja["procijenjena_potrošnja_goriva(kWh)"],
+            grouped_consumption["energy"],
             colors=self.colors,
-            labels=potrošnja["vrsta_prijevoza"],
+            labels=grouped_consumption.index,  # Use the index (vrsta_prijevoza) as labels
             autopct='%.0f%%'
         )
         plt.savefig(
@@ -111,8 +137,3 @@ class EnergyCharts:
             bbox_inches='tight'
         )
         return chart
-
-    def energy_per_capita(self):
-       toplinska_per_capita = self.heating_2011["potrošnja_toplinske_energije(kWh)"].sum() / self.n_residents_2011
-       print(toplinska_per_capita)
-
