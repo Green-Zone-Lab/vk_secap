bb4fd4008cd0be02ae79d519b0d788b67b95b3c2
dionizijefa <dionizije.fa@hotmail.com>
2023-08-09 23:38:22 +0200

    New constants and base inventory transport calculations

--- src/Constants.py
+++ src/Constants.py
@@ -2,9 +2,7 @@ from dataclasses import dataclass
 @dataclass
 class Constants:
     population_2011 = 35312
-    diesel_ton_to_litre = 1176.5
-    petrol_ton_to_litre = 1388.9
-    diesel_litre_to_kwh = 10.96
-    petrol_litre_to_kwh = 9.61
-    lpg_ton_to_litre = 1969
-    lpg_litre_to_kwh = 7.08
+    diesel_ton_mwh = 11.9
+    petrol_ton_mwh = 12.3
+    lpg_ton_mwh = 13.1
+    gas_ton_mwh = 13.3
