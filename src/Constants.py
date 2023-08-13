6dac2243b37771250cd322b03a86447ab1b797ea
dionizijefa <dionizije.fa@hotmail.com>
2023-08-13 20:34:15 +0200

    New data for co2 electricity emission generation

--- src/Constants.py
+++ src/Constants.py
@@ -1,4 +1,9 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from pathlib import Path
+
+import pandas as pd
+
+
 @dataclass
 class Constants:
     population_2011 = 35312
@@ -7,10 +12,22 @@ class Constants:
     lpg_ton_mwh = 13.1
     gas_ton_mwh = 13.3
 
-    co2_electricity_mwh_ton = 0.234
     co2_diesel_mwh_ton = 0.267
     co2_petrol_mwh_ton = 0.249
     co2_lpg_mwh_ton = 0.227
     co2_natgas_mwh_ton = 0.202
     co2_wood_mwh_ton = 0
     co2_heatoil_mwh_ton = 0.264
+
+    co2_electricity_mwh_ton_2011: float
+    co2_electricity_mwh_ton_2019: float
+
+    def __init__(self):
+        current_script_path = Path(__file__)
+        parent_dir = current_script_path.parents[1]
+        file_path = parent_dir / "data" / "JRC-COM-NEEFE_1990-2020.xlsx"
+
+        df = pd.read_excel(file_path, sheet_name=1, skiprows=1, engine='openpyxl')
+        # set the value for Croatia for the year 2011 and 2019
+        self.co2_electricity_mwh_ton_2011 = df.loc[df["Unnamed: 0"] == "Croatia", 2011].values[0]
+        self.co2_electricity_mwh_ton_2019 = df.loc[df["Unnamed: 0"] == "Croatia", 2019].values[0]
